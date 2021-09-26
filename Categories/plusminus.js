c9dbd13b29837169ea6f70f13c5fc057faba2724
nikitasiswadi <nikita535200029@stu.untar.ac.id>
2021-09-27 02:28:41 +0700

    hostingstart

---
+++ Categories/plusminus.js
@@ -0,0 +1,50 @@
+function add1(id){
+    console.log('i got clicked')
+    var jumlahstok = document.getElementById(id);
+    if(jumlahstok.value < 10){
+      jumlahstok.value++;
+    }
+}
+
+function minus1(id){
+    console.log('i got clicked')
+    var jumlahstok = document.getElementById(id);
+    if(jumlahstok.value > 1){
+      jumlahstok.value--;
+    }
+}
+
+function add2(id){
+    console.log('i got clicked')
+    var jumlahstok = document.getElementById(id);
+    if(jumlahstok.value < 10){
+      jumlahstok.value++;
+    }
+}
+
+function minus2(id){
+    console.log('i got clicked')
+    var jumlahstok = document.getElementById(id);
+    if(jumlahstok.value > 1){
+      jumlahstok.value--;
+    }
+}
+
+function add3(id){
+    console.log('i got clicked')
+    var jumlahstok = document.getElementById(id);
+    if(jumlahstok.value < 10){
+      jumlahstok.value++;
+    }
+}
+
+function minus3(id){
+    console.log('i got clicked')
+    var jumlahstok = document.getElementById(id);
+    if(jumlahstok.value > 1){
+      jumlahstok.value--;
+    }
+}
+
+feather.replace()
+
